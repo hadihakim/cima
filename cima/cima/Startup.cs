a69a51b3f550da197961045ac9f7e52d8f8e3463
hadihakim <hah092@usal.edu.lb>
2021-06-18 03:48:21 +0300

    first commit

---
+++ cima/cima/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cima.Startup))]
+namespace cima
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
